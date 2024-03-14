d187617727f54eeba977ce0769b3f9a1009db9da
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-14 12:38:02 +0100

    ADD: added the html select element. Added the js code to chose the different difficulties and change the grind depending on it

--- js/app.js
+++ js/app.js
@@ -1,20 +1,41 @@
 //l'utente clicca sul bottone play per generare una griglia di gioco
 //  - recupero l'elemento dal DOM tramite querySelector e la classe .play_btn
 const playButton = document.querySelector(".play_btn")
-//  - resto in ascolto del click su questo elemento ed al click stesso genero poi il campo di gioco
-playButton.addEventListener("click", function(){
-    
-    
-    // recupero l'elemento del DOM in cui inserisco gli oggetti che creerò (ovvero quello con la classe .grid) che chiamo gridElement
-    const gridElement = document.querySelector(".grid")
+// recupero l'elemento del DOM in cui inserisco gli oggetti che creerò (ovvero quello con la classe .grid) che chiamo gridElement
+const gridElement = document.querySelector(".grid")
 
+
+//recupero dal DOM l'elemento select dove l'utente sceglie la difficoltà
+const gameModesElement = document.getElementById("game_mode")
+
+
+
+
+
+//resto in ascolto del click sul playButton ed al click stesso genero poi il campo di gioco
+playButton.addEventListener("click", startGame)
+
+
+
+
+
+
+
+
+function startGame(){
+   
+    
+     
     //dichiaro una variabile size alla quale assegno il valore della grandezza di un lato della griglia di gioco (in questo caso 10)
     //ovvero 10 celle per lato, essendo la griglia di 10*10)
-    const size = 10
+    let size = getSize()
     //dichiaro una variabile numOfCells alla quale assegno il valore di size * size
     const numOfCells = size * size
 
-    
+
+    //inserisco una stringa vuota in modo tale da non reinserire un'ulteriore griglia al click del play, una volta che ho già cliccato
+    gridElement.innerHTML = ""
+
     //creo un ciclo for da iterare per creare le varie singole celle da inserire poi nel DOM
     for(let i = 0; i < numOfCells; i++){
 
@@ -35,7 +56,11 @@ playButton.addEventListener("click", function(){
         
         // inserisco nell'elemento recuperato dal DOM con classe grid, la variabile cellElement usando .append(cellElement)
         gridElement.append(cellElement)
-        
+
+
+        //aggiungo la classe che mi determina la dimensione delle celle
+        gridElement.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+
         
         //aggiungo l'event listener al click del cellElement
         cellElement.addEventListener("click", function(){
@@ -45,6 +70,27 @@ playButton.addEventListener("click", function(){
 
         })
         
-        
     }
-})
+}
+
+
+function getSize(){
+
+    let size = 10
+
+    //recupero la value delle options all'interno di questa select dichiarando una variabile ed assegnandola ad essa
+    let difficulty = gameModesElement.value
+
+    //SE la value === 3
+    if(difficulty === "3"){
+        size = 7
+    }else if(difficulty === "2"){
+    //ALTRIMENTI SE === 2
+        size = 9
+    }
+    
+      
+
+
+    return size
+}
